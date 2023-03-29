a2ffd6dd17d58adfdfb0173c3f0a3e21923f03ab
Christopherhalv0rsen <chrishp1997@hotmail.com>
2023-03-29 20:25:50 +0200

    .

--- filmsok/src/App.js
+++ filmsok/src/App.js
@@ -12,7 +12,7 @@ function App() {
   };
 
   async function info(sok) {
-    const response = await fetch(`https://www.omdbapi.com/?apikey=b56c90e7&s=${sok}`);    //forelesning fra foreleser om async og API-behandling 21/24/28.02
+    const response = await fetch(`https://www.omdbapi.com/?apikey=b56c90e7&s=${sok}`);    //forelesning fra foreleser om async og API-behandling 21/24/28.02 samt webtricks
     const data = await response.json();
     if (data.Search) {
       const filmPromises = data.Search.map (async (result) =>{
