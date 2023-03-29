a6fc282bb6db8cf016a708dee4e489dd8ccfca33
Christopherhalv0rsen <chrishp1997@hotmail.com>
2023-03-29 20:36:41 +0200

    Update App.js

--- filmsok/src/App.js
+++ filmsok/src/App.js
@@ -12,11 +12,11 @@ function App() {
   };
 
   async function info(sok) {
-    const response = await fetch(`https://www.omdbapi.com/?apikey=b56c90e7&s=${sok}`);    //forelesning fra foreleser om async og API-behandling 21/24/28.02 samt webtricks
+    const response = await fetch(`https://www.omdbapi.com/?apikey=b56c90e7&s=${sok}`);    //forelesning fra foreleser om async og API-behandling 21/24/28.02 
     const data = await response.json();
     if (data.Search) {
       const filmPromises = data.Search.map (async (result) =>{
-        const filmRespons = await fetch(`https://www.omdbapi.com/?apikey=b56c90e7&i=${result.imdbID}&plot=full`);
+        const filmRespons = await fetch(`https://www.omdbapi.com/?apikey=b56c90e7&i=${result.imdbID}&plot=full`);    //https://www.pluralsight.com/guides/process-an-api-response-in-react
         
 
         const filmData = await filmRespons.json();
